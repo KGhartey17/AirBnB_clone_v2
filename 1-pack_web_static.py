deabaec6fad6b31fabcfbc484d69ec600858794e
JeromeALX <kaphaelghartey@gmail.com>
2025-01-23 19:14:21 -0800

    exec

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python3
 """
-Fabric script to genereate tgz archive
+Fabric script to generate tgz archive
 execute: fab -f 1-pack_web_static.py do_pack
 """
 
 from datetime import datetime
-from fabric.api import *
+from fabric.api import local
 
 
 def do_pack():
     """
-    making an archive on web_static folder
+    Create an archive from the web_static folder.
     """
 
     time = datetime.now()
-    archive = 'web_static_' + time.strftime("%Y%m%d%H%M%S") + '.' + 'tgz'
-    local('mkdir -p versions')
-    create = local('tar -cvzf versions/{} web_static'.format(archive))
-    if create is not None:
-        return archive
+    archive = 'web_static_' + time.strftime("%Y%m%d%H%M%S") + '.tgz'
+    local('mkdir -p versions')  # Create the versions directory if it doesn't exist
+    create = local('tar -cvzf versions/{} web_static'.format(archive))  # Create the tarball
+
+    if create.succeeded:  # Check if the command was successful
+        return 'versions/' + archive
     else:
         return None
+
