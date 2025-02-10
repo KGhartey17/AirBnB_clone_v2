bac88d7788fcef518ace94221fabde9ac12bedc1
Jerome Kaphael Ghartey <gharteyjerome@gmail.com>
2025-02-10 03:13:28 +0000

    template

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,28 +1,33 @@
 #!/usr/bin/python3
-"""Starts a Flask web application to display states"""
+'''A simple Flask web application.
+'''
 from flask import Flask, render_template
+
 from models import storage
 from models.state import State
 
+
 app = Flask(__name__)
+'''The Flask application instance.'''
+app.url_map.strict_slashes = False
 
 
-@app.route("/states_list", strict_slashes=False)
+@app.route('/states_list')
 def states_list():
-    """
-    Display an HTML page with the list of all
-    State objects sorted by name.
-    """
-    states = storage.all(State).values()
-    sorted_states = sorted(states, key=lambda state: state.name)
-    return render_template("7-states_list.html", states=sorted_states)
+    '''The states_list page.'''
+    all_states = list(storage.all(State).values())
+    all_states.sort(key=lambda x: x.name)
+    ctxt = {
+        'states': all_states
+    }
+    return render_template('7-states_list.html', **ctxt)
 
 
 @app.teardown_appcontext
-def close_storage(exception):
-    """Close the SQLAlchemy session after each request"""
+def flask_teardown(exc):
+    '''The Flask app/request context end event listener.'''
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
